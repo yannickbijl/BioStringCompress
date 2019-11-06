c67abd2c4f37832f4aa3c48efa217f607587e891
Yannick Bijl <yannick.bijl@gmail.com>
2019-11-06 21:46:49 +0100

    Added class and placeholders.
    - Added class BinData for binary data.
    - Added placeholders for read and write methods.
    - Created global acces values data_filename, sequence, and binary.

--- SeqCompress/libs/lib_sequence_data.py
+++ SeqCompress/libs/lib_sequence_data.py
@@ -1,4 +1,24 @@
 class SeqData:
 
     def __init__(self, data_filename):
+        self.data_filename = data_filename
+        self.sequence = None
+
+    def read_data(self):
+        pass
+
+    def write_data(self):
+        pass
+
+
+class BinData:
+
+    def __init__(self, data_filename):
+        self.data_filename = data_filename
+        self.binary = None
+
+    def read_data(self):
+        pass
+
+    def write_data(self):
         pass
