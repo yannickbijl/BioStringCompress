19a00e38c763a0c11e3463636e68eaf97f684671
Yannick Bijl <yannick.bijl@gmail.com>
2019-11-06 21:39:23 +0100

    Expose the class BinData in lib_sequence_data.

--- SeqCompress/libs.py
+++ SeqCompress/libs.py
@@ -1,3 +1,3 @@
 import libs.lib_compress_algorithms as Compress
 import libs.lib_decompress_algorithms as Decompress
-from lib_sequence_data import SeqData
+from lib_sequence_data import SeqData, BinData
