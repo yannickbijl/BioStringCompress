736716e330cc97d7b6c065772397636d43656d74
Yannick Bijl <yannick.bijl@gmail.com>
2019-11-11 20:43:47 +0100

    Added encoding of nucleotide to binary and reverse
    - Also added type hinting.

--- SeqCompress/libs/lib_compress_algorithms.py
+++ SeqCompress/libs/lib_compress_algorithms.py
@@ -1,10 +1,15 @@
-def compress_4bit():
+encoding = {'A': '0000', 'C': '0001', 'G': '0010', 'T': '0011',
+            'R': '0100', 'Y': '0101', 'S': '0110', 'W': '0111',
+            'K': '1000', 'M': '1001', 'B': '1010', 'D': '1011',
+            'H': '1100', 'V': '1101', 'N': '1110', '.': '1111'}
+
+def compress_4bit(sequence: str): -> str
     pass
 
 
-def compress_5bit():
+def compress_5bit(sequence: str): -> str
     pass
 
 
-def compress_numeric():
+def compress_numeric(sequence: str): -> str
     pass
