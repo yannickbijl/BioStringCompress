8798896db24c476bbd6a7ccd4d13638929f8b1dc
Yannick Bijl <yannick.bijl@gmail.com>
2019-11-26 20:07:15 +0100

    Removed all code to start fresh.

--- SeqCompress/libs.py
+++
@@ -1,3 +0,0 @@
-import libs.lib_compress_algorithms as Compress
-import libs.lib_decompress_algorithms as Decompress
-from lib_sequence_data import SeqData, BinData
--- SeqCompress/libs/lib_logging.py
+++
@@ -1,5 +0,0 @@
-import logging
-import logging.config
-
-logging.config.fileConfig('configurations/logger.conf')
-logger = logging.getLogger('logtool')
