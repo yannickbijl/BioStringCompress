65d573f5fe756fa9d7d261173e697347281aa082
O. Veth <olga.veth@gmail.com>
2019-12-07 12:50:36 +0100

    Update lib_compress.py
    - Implemented conversion of base sequence string into a value [0-16] and converted into 4-bit string

--- BioStringCompress/lib_compress.py
+++ BioStringCompress/lib_compress.py
@@ -1,5 +1,29 @@
-def a():
-    return 1
+def strToIntDict(allBases):
+    #d = {'-': 1, 'A': 2, 'B': 3, 'C': 4, 'D': 5, 'G': 6, 'K': 7, 'M': 8, 
+    #     'N': 9, 'R': 10, 'S': 11, 'T': 12, 'V': 13, 'W': 14, 'Y': 15}
+    d = dict([(y,x+1) for x,y in enumerate(sorted(set(allBases)))])
+    return(d)
+
+def strToInt(sequenceBase, dictBases):
+    sequenceInt = []
+    for key in list(dictBases.keys()): 
+        for base in sequenceBase:
+            if base == key:
+                sequenceInt.append(dictBases[key])
+    return(sequenceInt)
+
+def bitConversion(intConvBase):
+    bitBase = '{:04b}'.format(intConvBase)
+    return(bitBase)
+    
+def method1(): # Previously defa
+    allBases = ["A", "C", "G", "T", "R", "Y", "S", "W", "K", "M", "B", "D", 
+                "M", "V", "N", "-"]
+    dictBases = strToIntDict(allBases)
+    #print(dictBases)
+    sequenceBases = "ACTG" # dummy sequence
+    #print(strToInt(sequenceBases, dictBases))
+
 
 
 def b():
