ecd6edf84ae12bfaba146c78d2953f86427d45d5
Yannick Bijl <yannick.bijl@gmail.com>
2020-01-15 20:01:43 +0100

    Made template write & read methods.
    - Check if path does exist, create if desired.
    - Set open-file statements to correct file opening modes.

--- BioStringCompress/lib_data.py
+++ BioStringCompress/lib_data.py
@@ -1,14 +1,37 @@
-def read_binary():
-    return 1
+import os
+
+def _check_pathdir_exist(filepath:str, create:bool = False):
+    dirpath = os.path.dirname(filepath)
+    if not os.path.isdir(dirpath):
+        if create:
+            os.makedirs(dirpath)
+        else:
+            raise FileNotFoundError
 
 
-def read_normal():
-    return 1
+def read_binary(filename:str):
+    _check_filepath_exist(filename)
+    with open(filename, 'rb') as datafile:
+        pass
+    return data
 
 
-def write_binary():
-    return 1
+def read_normal(filename:str):
+    _check_filepath_exist(filename)
+    with open(filename, 'r') as datafile:
+        pass
+    return data
 
 
-def write_normal():
-    return 1
+def write_binary(filename:str):
+    _check_filepath_exist(filename, create=True)
+    with open(filename, 'wb+') as datafile:
+        pass
+    return data
+
+
+def write_normal(filename:str):
+    _check_filepath_exist(filename, create=True)
+    with open(filename, 'w+') as datafile:
+        pass
+    return data
