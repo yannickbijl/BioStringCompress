3a2cc8fb8ab0891955f7aa03c2ee82faf69d0158
Yannick Bijl <yannick.bijl@gmail.com>
2020-01-15 20:50:40 +0100

    Small update.
    - Ensured that "no folders"  are accepted.
    - call correct function name _check_pathdir_exist
    - Small code formatting using newlines.

--- BioStringCompress/lib_data.py
+++ BioStringCompress/lib_data.py
@@ -2,34 +2,32 @@ import os
 
 def _check_pathdir_exist(filepath:str, create:bool = False):
     dirpath = os.path.dirname(filepath)
-    if not os.path.isdir(dirpath):
+    if not os.path.isdir(dirpath) and dirpath != "":
         if create:
             os.makedirs(dirpath)
         else:
-            raise FileNotFoundError
+            raise FileNotFoundError(f'{dirpath} does not exist.')
 
 
 def read_binary(filename:str):
-    _check_filepath_exist(filename)
+    _check_pathdir_exist(filename)
     with open(filename, 'rb') as datafile:
         data = datafile.read()
     return data
 
-
 def read_normal(filename:str):
-    _check_filepath_exist(filename)
+    _check_pathdir_exist(filename)
     with open(filename, 'r') as datafile:
         data = datafile.read()
     return data
 
 
 def write_binary(filename:str, data):
-    _check_filepath_exist(filename, create=True)
+    _check_pathdir_exist(filename, create=True)
     with open(filename, 'wb+') as datafile:
         datafile.write(data)
 
-
 def write_normal(filename:str, data):
-    _check_filepath_exist(filename, create=True)
+    _check_pathdir_exist(filename, create=True)
     with open(filename, 'w+') as datafile:
         datafile.write(data)
