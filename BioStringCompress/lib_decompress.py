0ad3e13218948215fbf9a62d9d01c856362bade7
Yannick Bijl <yannick.bijl@gmail.com>
2020-01-16 00:24:34 +0100

    Implemented binDecompress.
    - Added method _intToBaseDict
    - Added method _intToBase
    - Added method _intToBitConversion
    - Made binDecompress functional

--- BioStringCompress/lib_decompress.py
+++ BioStringCompress/lib_decompress.py
@@ -1,5 +1,24 @@
+def _intToBaseDict():
+    allBases = ["A", "C", "G", "T", "R", "Y", "S", "W", "K", "M", "B", "D", 
+                "H", "V", "N", "-"]
+    d = dict([(x,y) for x,y in enumerate(sorted(set(allBases)))])
+    # d = {0: '-', 1: 'A', 2: 'B', 3: 'C', 4: 'D', 5: 'G', 6: 'H', 7: 'K', 8: 'M', 9: 'N', 10: 'R', 11: 'S', 12: 'T', 13: 'V', 14: 'W', 15: 'Y'}
+    return d
+
+def _intToBase(integer:int, dictBases:dict):
+    return dictBases[integer]
+
+def _intToBitConversion(intConvBase: int):
+    return format(intConvBase, '#010b')[2:]
+
 def binDecompress(data:list = [28, 53]):
-    print(data)
+    seqList = []
+    dictBases = _intToBaseDict()
+    for byte in data:
+        byte = _intToBitConversion(byte)
+        seqList.append(int(byte[:4], 2))
+        seqList.append(int(byte[4:], 2))
+    return [_intToBase(base, dictBases) for base in seqList]
 
 
 def countDecompress(data:list = ['1A', '1T', '1C', '1G']):
