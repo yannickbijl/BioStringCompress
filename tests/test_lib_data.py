826a9c379e53e24fab70c9b4126c6d8cee85c83d
Yannick Bijl <yannick.bijl@gmail.com>
2020-01-21 19:35:48 +0100

    Setup tests for lib_data.

---
+++ tests/test_lib_data.py
@@ -0,0 +1,18 @@
+import pytest
+
+from BioStringCompress import lib_data
+
+def test__checkPathdirExist():
+    pass
+
+def test_readBinary():
+    pass
+
+def test_readNormal():
+    pass
+
+def test_writeBinary():
+    pass
+
+def test_writeNormal():
+    pass
